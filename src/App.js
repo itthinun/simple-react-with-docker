692ed261ad6f61486a2177bbe46d933c9820341f
bnDev <bnDev@dx.io>
2020-07-30 13:12:50 +0700

    Learnx react app.js change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learnx React
         </a>
       </header>
     </div>
