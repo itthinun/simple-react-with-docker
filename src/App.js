6fdb8b38cce6da17227ee16fbf49406c165756ab
bnDev <bnDev@dx.io>
2020-08-11 17:37:09 +0700

    Change App.js text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi ohio! ohioooooooo!!!!!!!
+          Hi ohio! ohioooooooo!!!!!!! Changes.
         </p>
         <a
           className="App-link"
